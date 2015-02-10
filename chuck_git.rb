68094c284df5d612e995288b6254ef93b9ee1f77
jenna <jenna.hildebrand@gmail.com>
2015-02-09 20:23:49 -0800

    no longer requires username/password, and has a method that accepts a username and a date to return activity for specified date

--- chuck_git.rb
+++ chuck_git.rb
@@ -3,15 +3,20 @@
 require 'rubygems'
 require 'mechanize'
 
-agent = Mechanize.new
+# let's assume today's date for now, but maybe later pass in a date as an argument
+date = Time.now.strftime("%Y-%m-%d")
 
-# get github.com/login
-# this will require user input of username/password - need to figure out how to handle that
-page = agent.get('http://github.com/login')
-# then find the un/pw form and assign it to a variable
-# since there are two forms with name 'nil' we need to find the second form by index
-# http://stackoverflow.com/questions/28017130/ruby-mechanize-find-form-w-no-name
-form = page.forms[1]
-form.login = 'user_input'
-form.password = 'encrypted_somehow_user_input'
-page = agent.submit(form)
+# need to get usernames from a csv
+def get_file
+end
+
+def commits(username, date)
+	agent = Mechanize.new
+# get github.com/user
+# url format: http://github.com/rejennarate?tab=contributions&from=2015-02-04
+  page = agent.get("http://github.com/#{username}?tab=contributions&from=#{date}")
+  # well this is super bold, let's hope the structure of this page never changes at all
+  # daily activity is in array index 22
+  puts page.links[22]
+end
+
