19e6596133f865c5eadaf9accbe5f51bedffd726
jenna <jenna.hildebrand@gmail.com>
2015-02-09 23:40:55 -0800

    script now accepts a csv input and outputs accusatorily. and the formatting is shameful.

--- chuck_git.rb
+++ chuck_git.rb
@@ -1,22 +1,50 @@
 #!/usr/bin/ruby
 
+
 require 'rubygems'
 require 'mechanize'
 
 # let's assume today's date for now, but maybe later pass in a date as an argument
 date = Time.now.strftime("%Y-%m-%d")
 
-# need to get usernames from a csv
-def get_file
+def file_input
+  if ARGV.empty?
+    puts 'please provide a .csv file.'
+  elsif ARGV.length > 1
+    puts 'too many arguments provided. please provide a single file'
+  else
+    ARGV[0]
+  end
 end
 
-def commits(username, date)
+# get usernames from a csv
+# even though right now each username will be a separate line,
+# maybe someday we will add some parameters and separate by commas.
+def load_users 
+	File.readlines(file_input).each do |line|
+		usernames = line.split(",")
+		# the return value of this method needs to be an array, not the file
+		# so there's an explicit return
+		return usernames
+	end
+end
+
+def commits(date)
+	puts "Today's date: #{date}"
 	agent = Mechanize.new
-# get github.com/user
-# url format: http://github.com/rejennarate?tab=contributions&from=2015-02-04
-  page = agent.get("http://github.com/#{username}?tab=contributions&from=#{date}")
-  # well this is super bold, let's hope the structure of this page never changes at all
-  # daily activity is in array index 22
-  puts page.links[22]
+  # get github url and substitute in a username and date
+  load_users.each do |username|
+    page = agent.get("http://github.com/#{username.chomp}?tab=contributions&from=#{date}")
+ 
+	  # well this is super bold, let's hope the structure of this page never changes at all
+	  # daily activity is in array index 22
+	  green_bar = page.links[22].to_s
+	  if green_bar.include?('Pushed')
+	  	puts "#{username} #{green_bar}"
+	  else
+	  	puts "#{username} did not push any commits. Perhaps they need...motivation."
+	  end
+	end
 end
 
+commits(date)
